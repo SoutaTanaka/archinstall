841e45b5b69059125cdb18d0f58e98952af80342
SoutaTanaka <tanakakesouta@yahoo.co.jp>
2018-07-20 16:33:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,23 +21,28 @@ visudo
 
 pacman -Syu
 
-pacman -S xorg-server xorg-apps xorg-xinit xorg-xmodmap  xorg-xclock xterm  sudo grub-efi-x86_64 qtcreator boost opencv vim xfce4-goodies xfce4 lightdm lightdm-gtk-greeter flashplugin otf-ipafont fcitx-im fcitx-configtool fcitx-mozc xdg-user-dirs-gtk grub os-prober dosfstools xf86-input-synaptics xf86-video-intel  mesa iw wpa_supplicant dialog rxvt-unicode zsh tmux i3 vlc dmenu clang gcc openssh git openssl libreoffice libreoffice-ja blueman powertop  tree  alsa-utils tig  source-highlight unzip htop dstat iotop networkmanager screenfetch chromium sakura sl gnome
+
+pacman -S xorg-server xorg-apps xorg-xinit xorg-xmodmap  xorg-xclock xterm  sudo grub-efi-x86_64 qtcreator boost opencv vim lightdm lightdm-gtk-greeter flashplugin otf-ipafont fcitx-im fcitx-configtool fcitx-mozc xdg-user-dirs-gtk grub os-prober dosfstools xf86-input-synaptics xf86-video-intel  mesa iw wpa_supplicant dialog rxvt-unicode zsh tmux i3 vlc dmenu clang gcc openssh git openssl libreoffice libreoffice-ja blueman powertop  tree  alsa-utils tig  source-highlight unzip htop dstat iotop networkmanager screenfetch chromium sakura
 
 systemctl enable lightdm.service
 
 echo 'KEYMAP=jp106' >> /etc/vconsole.conf
 
-vim /etc/mkinitcpio.conf
+echo 'Section "InputClass"'  >> /etc/X11/xorg.conf.d/10-keyboard.conf
+echo 'Identifier "Keyboard Defaults"'  >> /etc/X11/xorg.conf.d/10-keyboard.conf
+echo 'MatchIsKeyboard "yes"'  >> /etc/X11/xorg.conf.d/10-keyboard.conf
+echo 'Option "XkbLayout" "jp"'  >> /etc/X11/xorg.conf.d/10-keyboard.conf
+echo 'EndSection'  >> /etc/X11/xorg.conf.d/10-keyboard.conf
 
-mkinitcpio -p linux
+pacman -S adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
 
-vim /etc/default/grub
+pacman -S intel-ucode efibootmgr dosfstools
 
-git clone http://github.com/Shougo/neobundle.vim $HOME
-
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinuxGrub --recheck
+mkdir /boot/EFI/boot
+cp /boot/EFI/ArchLinuxGrub/grubx64.efi /boot/EFI/boot/bootx64.efi
 grub-mkconfig -o /boot/grub/grub.cfg
-grub-mkstandalone -o boot.efi -d /usr/lib/grub/x86_64-efi -O x86_64-efi --compress=xz /boot/grub/grub.cfg
 
 mkdir /mnt/usb
+
+
